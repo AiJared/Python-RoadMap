825103ae4be5d179c8b1ddcd4e635ce0f8d3a51a
Jared <ojared511@gmail.com>
2021-11-26 09:17:52 +0300

    Python roadmap

---
+++ Indentation_Intro.py
@@ -0,0 +1,19 @@
+if 3<9:
+    print ("cool")
+phone="oppo"
+print(phone)
+firsName="davie"
+print(firsName)
+a="john"
+A="dot"
+print(a)
+print(A)
+itsRaining=True
+print(itsRaining)
+print(type(itsRaining))
+name=input("mtapeli")
+print(name)
+university=input("Enter your University")
+print(university)
+course=input("Enter your course")
+print(course)
---
+++ Python Shell.py
@@ -0,0 +1,5 @@
+# A python shell is an application that is installed whenever you install python on
+# your machine.
+# It is an evironment where one can run and quickly check some python codes
+# You can code nearly everything in the shell but it is not advisable for normal projects
+# This is because you can't save your work and keep track of your progress.
