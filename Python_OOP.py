0274894bb4afb971a773c270adfba85a1af4af47
Kerriea-star <ojared511@gmail.com>
2022-01-17 09:09:49 +0300

    Documentation in Object Oriented Programming in python

--- Python_OOP.py
+++ Python_OOP.py
@@ -1,26 +1,29 @@
 import math
 
 class Point:
+    'Represents a point in two dimensional geometric coordinates'
+
+    def __init__(self, x=0, y=0):
+        '''Initializes the position of a new point. The x and y coordinates can be specified
+        . If they are not, the point defaults to the origin.'''
+        self.move(x, y)
+
     def move(self, x, y):
+        "Move the point to a new location in 2D space"
         self.x = x
         self.y = y
-
-    def reset(self):
-        self.move(0, 0)
     
+    def reset(self):
+        'Reset the point back to the geometric origin: 0, 0'
+        self.move(0, 0)
+
     def calculate_distance(self, other_point):
+        """Calculate distance from this point to a second 
+        point passed as a parameter
+        
+        This Function uses the pythagorean theorem to calculate the diatance between
+        the two points. The distance is 
+        returned as a float."""
         return math.sqrt(
             (self.x - other_point.x) ** 2 +
             (self.y - other_point.y) ** 2)
-# How to use it
-point1 = Point()
-point2 = Point()
-
-point1.reset()
-point2.move(5, 0)
-print(point2.calculate_distance(point1))
-assert (point2.calculate_distance(point1) == 
-       point1.calculate_distance(point2))
-point1.move(3,4)
-print(point1.calculate_distance(point2))
-print(point1.calculate_distance(point1))
