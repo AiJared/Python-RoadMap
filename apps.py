d6c9bc468846c3f77b3cbc0a99d9813c21cf11b2
Kerriea-star <ojared511@gmail.com>
2022-02-02 16:01:26 +0300

    More on dictionaries

--- apps.py
+++ apps.py
@@ -1,87 +1,135 @@
 # Python Dictionaries
 """
-Collection types that store data in terms of key and value.
-Dictionaries are orderd, changeable and does not allow duplicates.
-Python 3.7 and later versions are orderd while 3.6 and earlier vasions are unorderd.
+Dictionaries are also python collection types just like lists and tuples
+They store multiple data under one variable in the form of key:value.
+They orderd, changeable and do not allow duplicates
+Orderd means that they contain a particular form of arrangement. And this is most popular in python versions 3.7 to date.
+But python versions 3.6 and earlier are unorderd meaning that they cannot be accessed using index numbers.
 """
-car = {
-    "brand": "Ford",
-    "model": "Model X",
-    "year": 2012,
-}
-print(car)
-print(type(car))
-# We can output a value by specifying it's key
-print(car["brand"])
 
-# we can output a list of keys using the keys() method
-x = car.keys()
-print(x)
-# We can change the values of keys by specifying the keys and the values
-car["year"] = 2020
-print(car["year"])
-# we can also use the update() to make changes to a dictionary
-car.update({"year": 2018})
-print(car)
-# We can use the values() method to output a list of all values in a dictionary
-a = car.values()
-print(a)
-# we use items() methods to get a list of all the items in a dictionary
-c = car.items()
-print(c)
-# we can use get() method to get the values a specific keys
-b = car.get("model")
-print(b)
-# Add an item using the update method
-car.update({"color": "red"})
-print(car)
-# Using the pop() with a specifeid key to remove an item
-car.pop("color")
-print(car)
-# Using popitem() to remove the last item inserted in our dictionary
-car.popitem()
-print(car)
-
-student  = {
-    "name": "Jared",
-    "RegNo": "Ic211",
+student = {
+    "name": "Kim",
+    "RegNo": "TU01IC211",
     "age": 20,
-    "year": 2021
+    "period": 4,
+    "period": 5
 }
-# Using del keyword to remove a specific item using a specified key
-del student["year"]
 print(student)
-# Using clear method to make the dictionary empty
-student.clear()
+print(type(student))
+
+# Get the specific values from a dictionary by specifying the key
+print(student["RegNo"])
+
+# Get a specific value of from a dictionary using the get() method
+x = student.get("RegNo")
+print(x)
+
+# Get all the values in form of a list using the values() method
+a = student.values()
+print(a)
+
+# Get all the keys from a dictionary using the keys() method
+b = student.keys()
+print(b)
+
+# Get all the items of a dictionary by using the items() method
+c = student.items()
+print(c)
+
+# Adding an item in a dictionary using the update() method
+student.update({"Weight": 69})
+print(student)
+student.update({"Height": 170})
 print(student)
 
-# Nested Dictionaries
+# Change a specific value in the dictionary by specifying it's key
+student["age"] = 21
+print(student)
+
+# Removing a specific item in a dictionary using the pop() method by specifying it's key
+student.pop("Height")
+print(student)
+# Removing the last item in a dictionary using the popitem() method
+student.popitem()
+print(student)
+# Remove a specific item in a dictionary using del keyword by specifying it's key
+del student["period"]
+print(student)
+
+# Remove the entire dictionary by using del keyword
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+print(thisdict)
+print(type(thisdict))
+
+# Remove everything from a dictionary by using clear()
+thisdict.clear()
+print(thisdict)
+
+# Nested dictionaries
 """
-A dictionary can contain dictionaries as show in the programs below.
+A dictionary can contain other dictionaries. 
+You can create a dictionary and create other small dictionaries inside that dict.
 """
-# We can aslo create dictionaries outside and create another big dictionary that will contain all of them
+vehicles = {
+    "vehicle1": {
+        "brand": "Tesla car",
+        "model": "Model X",
+        "year": 2021
+    },
+    "vehicle2": {
+        "brand": "Toyota",
+        "model": "Fielder",
+        "year": 2015
+    },
+    "vehicle3": {
+        "brand": "Subaru",
+        "model": "X6",
+        "year": 2019
+    }
+}
+
+print(vehicles)
+print(type(vehicles))
+print(vehicles["vehicle1"])
+a = vehicles.keys()
+print(a)
+
+# Create dictionaries and put them inside one large dictionary that can contain all of them
 child1 = {
-    "name": "Emil",
-    "year": 2004
+    "name": "Kim",
+    "year": 2000
 }
 
 child2 = {
-    "name": "Tobias",
-    "year": 2007
+    "name": "Kate",
+    "year": 2003
 }
 
 child3 = {
-    "name": "Linus",
-    "year": 2011
+    "name": "Tim",
+    "year": 2007
 }
 
-myfamily = {
+mother = {
+    "name": "Tracy",
+    "year": 1978
+}
+
+father = {
+    "name": "Chandler",
+    "year": 1972
+}
+
+Family = {
     "child1": child1,
     "child2": child2,
-    "child3": child3
+    "child3": child3,
+    "Mother": mother,
+    "Father": father
 }
-
-print(child1)
-print(child2)
-print(child3)
-print(myfamily)
+print(Family)
+print(type(Family))
