114ec4d91fd06d83a20cdfc0f13eca8cf4d2b5ad
Kerriea-star <ojared511@gmail.com>
2024-07-14 10:40:42 +0300

    next method

--- OOP/sequence_iterator.py
+++ OOP/sequence_iterator.py
@@ -9,4 +9,15 @@ class SequenceIterator:
         """
         self._seq = sequence # keep a reference to the underlying data
         self._k = -1 # will increment to 0 on the first call to nec
+
+    def __next__(self):
+        """
+        Return the next element or else raise StopIteration error.
+        """  
+        self._k += 1 # advance to the next index
+        if self._k < len(self._seq):
+            return self._seq[self._k] # return the data element
+        else:
+            raise StopIteration()
         
+    
