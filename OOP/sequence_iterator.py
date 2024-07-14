22f11dc21c332531459bd5f921d64a1e3eae4f48
Kerriea-star <ojared511@gmail.com>
2024-07-14 10:38:02 +0300

    Sequence iterator class constructor method

--- OOP/sequence_iterator.py
+++ OOP/sequence_iterator.py
@@ -2,4 +2,11 @@ class SequenceIterator:
     """
     An iterator for any Python's sequence types.
     """
-    
+
+    def __init__(self, sequence):
+        """
+        Create an iterator for the given sequence.
+        """
+        self._seq = sequence # keep a reference to the underlying data
+        self._k = -1 # will increment to 0 on the first call to nec
+        
