5670b973dea3a7e413141446d765e3723feb20bb
Kerriea-star <ojared511@gmail.com>
2024-07-15 10:45:49 +0300

    getitem method to return entry at index k using standard interpretation if negative

--- OOP/range.py
+++ OOP/range.py
@@ -29,4 +29,15 @@ class Range:
         """
         return self._length
     
-    
+    def __getitem__(self, k):
+        """
+        Return entry at index k (using standard interpretation if negative).
+        """
+
+        if k < 0:
+            k += len(self)
+        
+        if not 0 <= k < self._length:
+            raise IndexError("index out of range")
+        
+        return self._start + k * self._step
