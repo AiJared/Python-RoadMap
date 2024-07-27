fd884a42a45d1f555e3315293a3fbda1847dc342
Kerriea-star <ojared511@gmail.com>
2024-07-27 12:19:31 +0300

    generic progression, advance menthod

--- OOP/numeric_progression.py
+++ OOP/numeric_progression.py
@@ -8,3 +8,14 @@ class Progression:
         Initialize current to the first value of the progression
         """
         self._current = start
+
+    def _advance(self):
+        """
+        Update self._current to a new value.
+
+        This should be overridded by a subclass to customize progression.
+        
+        By convetion, if current is set to None, this designates the
+        end of a finite progression.
+        """
+        self._current += 1
