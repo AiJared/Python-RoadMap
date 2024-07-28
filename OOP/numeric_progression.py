3ca8bf28e06d8b19f836584321b405ab1f7f3150
Kerriea-star <ojared511@gmail.com>
2024-07-28 22:11:01 +0300

    testing the progression class

--- OOP/numeric_progression.py
+++ OOP/numeric_progression.py
@@ -108,4 +108,25 @@ class FibonacciProgression(Progression):
         update current value by taking the sum of the previous two.
         """
         self._prev, self._current = self._current, self._prev + self._current
-        
+
+if __name__ == '__main__':
+    print("Default Progression:")
+    Progression().print_progression(10)
+
+    print("Arithmetic progression with increment 5:")
+    ArithmeticProgression(5).print_progression(10)
+
+    print("Arithmetic progression with increment 5 and start 2:")
+    ArithmeticProgression(5, 2).print_progression(10)
+
+    print("Geometric progression with default base:")
+    GeometricProgression().print_progression(10)
+
+    print("Geometric progression with base 3:")
+    GeometricProgression(3).print_progression(10)
+
+    print("Fibonacci progression with default start values:")
+    FibonacciProgression().print_progression(10)
+
+    print("Fibonacci progression with start values 4 and 6:")
+    FibonacciProgression(4, 6).print_progression(10)
