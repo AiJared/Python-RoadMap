392a6d8a19bf0f210110d91c50f3e0efd9d97098
Kerriea-star <ojared511@gmail.com>
2024-08-29 06:57:54 +0300

    function to return the middle letter of a string

--- challenges/middle_letter.py
+++ challenges/middle_letter.py
@@ -7,3 +7,12 @@ If there is no middle letter, your function should return the empty string.
 
 For example, mid("abc") should return "b" and mid("aaaa") should return "".
 """
+
+def mid(value):
+    if len(value) %2 == 0:
+        return " "
+    else:
+        middle_index = len(value) // 2
+        return value[middle_index]
+print(mid("abc"))
+print(mid("aaaa"))
