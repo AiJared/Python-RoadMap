a86d3e33c14a012a4913e2bcd4f38a25b573eefe
Kerriea-star <ojared511@gmail.com>
2024-08-29 06:52:42 +0300

    function to return the middle letter of a string

---
+++ challenges/middle_letter.py
@@ -0,0 +1,9 @@
+# Middle letter
+
+"""
+Write a function named mid that takes a string as its parameter. 
+Your function should extract and return the middle letter. 
+If there is no middle letter, your function should return the empty string.
+
+For example, mid("abc") should return "b" and mid("aaaa") should return "".
+"""
