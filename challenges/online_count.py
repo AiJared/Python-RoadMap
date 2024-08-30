7dc6bfbeb483c6128d8632c8a9a919d468eadaea
Kerriea-star <ojared511@gmail.com>
2024-08-30 09:07:23 +0300

    function to check the online status and return the number of people who are online from a dictionary

---
+++ challenges/online_count.py
@@ -0,0 +1,18 @@
+# Online status
+
+"""
+The aim of this challenge is, given a dictionary of people's online status, to count the number of people who are online.
+
+For example, consider the following dictionary:
+
+statuses = {
+    "Alice": "online",
+    "Bob": "offline",
+    "Eve": "online",
+}
+In this case, the number of people online is 2.
+
+Write a function named online_count that takes one parameter. The parameter is a dictionary that maps from strings of names to the string "online" or "offline", as seen above.
+
+Your function should return the number of people who are online
+"""
