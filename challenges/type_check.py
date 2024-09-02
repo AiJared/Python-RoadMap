6c2043fdccd21a57f71491629afa70f53d783cb0
Kerriea-star <ojared511@gmail.com>
2024-09-02 08:32:49 +0300

    function to check the type of value

---
+++ challenges/type_check.py
@@ -0,0 +1,7 @@
+# Type check
+
+"""
+Write a function named only_ints that takes two parameters. Your function should return True if both parameters are integers, and False otherwise.
+
+For example, calling only_ints(1, 2) should return True, while calling only_ints("a", 1) should return False.
+"""
