a1104d02025397116c4af01748fb21741ae00d80
Kerriea-star <ojared511@gmail.com>
2024-09-02 08:33:31 +0300

    function to check the type of value

--- challenges/type_check.py
+++ challenges/type_check.py
@@ -5,3 +5,11 @@ Write a function named only_ints that takes two parameters. Your function should
 
 For example, calling only_ints(1, 2) should return True, while calling only_ints("a", 1) should return False.
 """
+
+def only_ints(inta, intb):
+    if isinstance(inta, int) and isinstance(intb, int):
+        return True
+    else:
+        return False
+    
+print(only_ints(1, 2))
