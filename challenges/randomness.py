533369fbcc24adbb29a303b175aaba92af0c425e
Kerriea-star <ojared511@gmail.com>
2024-09-02 08:17:05 +0300

    function to generate a random integer between one and hundred

---
+++ challenges/randomness.py
@@ -0,0 +1,9 @@
+# Randomness
+"""
+Define a function, random_number, that takes no parameters. 
+The function must generate a random integer between 1 and 100, both inclusive, and return it.
+
+Calling the function multiple times should (usually) return different numbers.
+
+For example, calling random_number() some times might first return 42, then 63, then 1.
+"""
