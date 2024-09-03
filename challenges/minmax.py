93263ef73510812c70923f6ea288fb239ed40185
Kerriea-star <ojared511@gmail.com>
2024-09-03 22:09:08 +0300

    functions that check if a number is even or not

---
+++ challenges/minmax.py
@@ -0,0 +1,14 @@
+"""
+Write a short Python function, minmax(data), that takes a sequence of
+one or more numbers, and returns the smallest and largest numbers, in the
+form of a tuple of length two. Do not use the built-in functions min or
+max in implementing your solution
+"""
+# Using min() and max()
+
+# def minmax(data):
+#     for i, j in data:
+#         if i < range(data) and j > range(data):
+#             return (i, j)
+        
+# print(minmax(data = [1,2,3,4]))
