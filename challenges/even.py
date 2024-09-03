e8c5ec24a67f7a7149f6a8d803011dc64e6a9145
Kerriea-star <ojared511@gmail.com>
2024-09-03 08:36:47 +0300

    function that checks if an integer is even or not using modulo

--- challenges/even.py
+++ challenges/even.py
@@ -4,3 +4,11 @@ returns True if k is even, and False otherwise. However, your function
 cannot use the multiplication, modulo, or division operators
 """
 
+# With modulo
+def even(k):
+    if k%2 == 0:
+        return True
+    else:
+        return False
+    
+print(even(7))
