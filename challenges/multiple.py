58189e30d439ddbd0da63a29129852c8398c5ee2
Kerriea-star <ojared511@gmail.com>
2024-09-03 08:22:47 +0300

    function that checks if two integers are multiples of each other

--- challenges/multiple.py
+++ challenges/multiple.py
@@ -3,3 +3,11 @@ Write a short Python function, is multiple(n, m), that takes two integer
 values and returns True if n is a multiple of m, that is, n = mi for some
 integer i, and False otherwise.
 """
+def is_multiple(n, m):
+    i = 2
+    if n / m == i:
+        return True
+    else: 
+        return False
+
+print(is_multiple(9, 3))
