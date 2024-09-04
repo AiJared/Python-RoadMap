f2c1dc6704edfef20c3ddec5b3de08d917ed508f
Kerriea-star <ojared511@gmail.com>
2024-09-04 10:39:30 +0300

    function to check the minimum and maximum values in a list then return them

--- challenges/minmax.py
+++ challenges/minmax.py
@@ -4,11 +4,16 @@ one or more numbers, and returns the smallest and largest numbers, in the
 form of a tuple of length two. Do not use the built-in functions min or
 max in implementing your solution
 """
-# Using min() and max()
 
-# def minmax(data):
-#     for i, j in data:
-#         if i < range(data) and j > range(data):
-#             return (i, j)
-        
-# print(minmax(data = [1,2,3,4]))
+def minmax(data):
+    
+    smallest = largest = data[0]
+
+    for num in data:
+        if num < smallest:
+            smallest = num
+        if num > largest:
+            largest = num
+    return (smallest, largest)
+
+print(minmax([1,2,3,4]))
