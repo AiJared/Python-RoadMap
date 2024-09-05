8a5174bd548a00e36bc9de993ae33c7c015de8bb
Kerriea-star <ojared511@gmail.com>
2024-09-05 09:01:15 +0300

    function to return the sum of squares of positive integers below given integer

--- challenges/squares.py
+++ challenges/squares.py
@@ -4,10 +4,10 @@ the sum of the squares of all the positive integers smaller than n.
 """
 
 def squares(n):
-    total = 0
+    total = 0                   # initialize a variable to store the sum of squares
 
-    for num in range(1, n):
-        total += num ** 2
-    return total
+    for num in range(1, n):     # Iterate over all numbers less than n
+        total += num ** 2       # Add the square of num to total
+    return total                # return the accumulated total
 
 print(squares(5))
