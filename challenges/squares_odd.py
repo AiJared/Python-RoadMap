40a4f9d3c5b12d97a4bc8bc219b689d51f9a84d2
Kerriea-star <ojared511@gmail.com>
2024-09-05 09:19:02 +0300

    function to return the sum of squares of positive odd integers less than the given integer

--- challenges/squares_odd.py
+++ challenges/squares_odd.py
@@ -2,3 +2,13 @@
 Write a short Python function that takes a positive integer n and returns
 the sum of the squares of all the odd positive integers smaller than n.
 """
+
+def squares(n):
+    total = 0
+
+    for num in range(1, n):
+        if num%2 != 0:
+            total += num ** 2
+    return total
+
+print(squares(5))
