b310b468664ef0b2352c1f0fff59569f4dbcc409
Kerriea-star <ojared511@gmail.com>
2024-09-05 09:23:57 +0300

    function to return the sum of squares of positive odd integers less than the given integer

--- challenges/squares_odd.py
+++ challenges/squares_odd.py
@@ -4,11 +4,11 @@ the sum of the squares of all the odd positive integers smaller than n.
 """
 
 def squares(n):
-    total = 0
+    total = 0                     # initialize a variable for storing sum of squares
 
-    for num in range(1, n):
-        if num%2 != 0:
-            total += num ** 2
-    return total
+    for num in range(1, n):       # iterate over all positive integers less than n
+        if num % 2 != 0:          # check for positive odd numbers
+            total += num ** 2     # sum up the squares of all the positive odd numbers less than n
+    return total                  # return the cummulated total
 
 print(squares(5))
