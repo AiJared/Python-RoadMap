4d29703869b25ab000731f39a5d53a5978e341a3
Kerriea-star <ojared511@gmail.com>
2024-09-07 09:17:08 +0300

    sets

--- sets.py
+++ sets.py
@@ -8,3 +8,11 @@ This means that:
 
 """
 
+# Key Properties of a Set:
+"""
+1. Unique - All the elements of a set are not the same. If you try to an existing element it
+won't be added.
+2. Unordered - All the elements in a set are not in any order. Meaning that you cannot rely
+on order when iterating through a set or printing its elements.
+3. Mutable - You can add or remove elements from a set.
+"""
