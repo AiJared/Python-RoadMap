8f6654f363a61dd96266363c7d52ab3b2410007f
Kerriea-star <ojared511@gmail.com>
2024-09-07 08:55:55 +0300

    function that determines if all the numbers in a sequence are different from each other

--- challenges/creativity/distinct_numbers.py
+++ challenges/creativity/distinct_numbers.py
@@ -2,3 +2,10 @@
 Write a Python function that takes a sequence of numbers and determines
 if all the numbers are different from each other (that is, they are distinct).
 """
+
+"""
+The simplest way of doing this is by comparing the length of the list and the length of the set
+created from that list. Since set cannot contain duplicate values, if the lengths are the same
+then the numbers are different.
+"""
+
