6018d559bec25f293c8821aaee6744dcf176679a
Kerriea-star <ojared511@gmail.com>
2024-09-07 09:13:37 +0300

    sets

---
+++ sets.py
@@ -0,0 +1,10 @@
+# What is a set
+"""
+A set is an unorderd sequence/collection of unique/distinct items.
+This means that:
+
+1. Set does not have duplicates
+2. It is unordered meaning that you cannot access items using index numbers as you would in list
+
+"""
+
