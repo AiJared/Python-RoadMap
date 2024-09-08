90810f055f6acc0a5fd355c9f7c736842e1b09bd
Kerriea-star <ojared511@gmail.com>
2024-09-08 09:03:41 +0300

    list comprehension for alphabets

--- challenges/creativity/list_comprehension_str.py
+++ challenges/creativity/list_comprehension_str.py
@@ -3,3 +3,8 @@ Demonstrate how to use Python’s list comprehension syntax to produce
 the list [ a , b , c , ..., z ], but without having to type all 26 such
 characters literally.
 """
+
+"""
+Use the the chr() function, which converts ASCII values into their corresponding characters. 
+The ASCII values for lowercase letters 'a' to 'z' are from 97 to 122.
+"""
