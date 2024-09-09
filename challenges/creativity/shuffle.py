ed1537c6e3fa20509b217038a631f4e79ebdab98
Kerriea-star <ojared511@gmail.com>
2024-09-09 08:44:57 +0300

    using randint to implement shuffle function

---
+++ challenges/creativity/shuffle.py
@@ -0,0 +1,8 @@
+"""
+Python’s random module includes a function shuffle(data) that accepts a
+list of elements and randomly reorders the elements so that each possible order occurs with equal probability. 
+The random module includes a 
+more basic function randint(a, b) that returns a uniformly random integer
+from a to b (including both endpoints). Using only the randint function,
+implement your own version of the shuffle function.
+"""
