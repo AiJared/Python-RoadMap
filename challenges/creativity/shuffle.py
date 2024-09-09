d83a6087353575d2eccf443e2291ff8ce8cf0de5
Kerriea-star <ojared511@gmail.com>
2024-09-09 08:49:49 +0300

    using randint to implement shuffle function

--- challenges/creativity/shuffle.py
+++ challenges/creativity/shuffle.py
@@ -6,3 +6,11 @@ more basic function randint(a, b) that returns a uniformly random integer
 from a to b (including both endpoints). Using only the randint function,
 implement your own version of the shuffle function.
 """
+
+# Step by step strategy
+"""
+1. Start by iterating through the list
+2. For each element in the list (at index i) generate a random index j between in and the end of the list.
+3. Swap the element at index i with the element at index j
+4. Continue this process until all the elements have been shuffled.
+"""
