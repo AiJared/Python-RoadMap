4d074d2a00e97fafa014dbf7687f27d84cb832c7
Kerriea-star <ojared511@gmail.com>
2024-09-11 23:55:56 +0300

    function for reading lines from standard input

--- challenges/creativity/standard_input.py
+++ challenges/creativity/standard_input.py
@@ -3,3 +3,4 @@ Write a Python program that repeatedly reads lines from standard input
 until an EOFError is raised, and then outputs those lines in reverse order
 (a user can indicate end of input by typing ctrl-D).
 """
+input_value = input("Enter an input: ")
