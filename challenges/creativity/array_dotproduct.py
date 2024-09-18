63d61b47ba20239870a38660df01e3c1d8e4382b
Kerriea-star <ojared511@gmail.com>
2024-09-18 09:56:22 +0300

    dot product of arrays

--- challenges/creativity/array_dotproduct.py
+++ challenges/creativity/array_dotproduct.py
@@ -7,10 +7,12 @@ an array c of length n such that c[i] = a[i] · b[i], for i = 0,...,n− 1
 def array_dotproduct():
     a = [1,2,3,4,5]
     b = [6,7,8,9,10]
-
+    # initialize an empty list to store the dot product
     c = []
 
+    # loop through the indices of both arrays
     for i in range(len(a)):
+        # multiply the corresponding elements and append them to c
         c.append(a[i] * b[i])
     return c
 
