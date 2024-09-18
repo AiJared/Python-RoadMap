32ebce0be2aa4f38d3accc0ea447583a853941ba
Kerriea-star <ojared511@gmail.com>
2024-09-18 09:51:11 +0300

    dot product of arrays

--- challenges/creativity/array_dotproduct.py
+++ challenges/creativity/array_dotproduct.py
@@ -3,3 +3,15 @@ Write a short Python program that takes two arrays a and b of length n
 storing int values, and returns the dot product of a and b. That is, it returns
 an array c of length n such that c[i] = a[i] · b[i], for i = 0,...,n− 1
 """
+
+def array_dotproduct():
+    a = [1,2,3,4,5]
+    b = [6,7,8,9,10]
+
+    c = []
+
+    for i in range(len(a)):
+        c.append(a[i] * b[i])
+    return c
+
+print(array_dotproduct())
