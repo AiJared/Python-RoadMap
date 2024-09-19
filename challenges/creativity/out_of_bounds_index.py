3b518010c9cebcad09360cbe4a27a3054288b1bc
Kerriea-star <ojared511@gmail.com>
2024-09-19 09:50:07 +0300

    out of bound index

--- challenges/creativity/out_of_bounds_index.py
+++ challenges/creativity/out_of_bounds_index.py
@@ -1,5 +1,6 @@
 """
-Give an example of a Python code fragment that attempts to write an element to a list based on an index that may be out of bounds. If that index
+Give an example of a Python code fragment that attempts to write an element to 
+a list based on an index that may be out of bounds. If that index
 is out of bounds, the program should catch the exception that results, and
 print the following error message:
 “Don’t try buffer overflow attacks in Python!
