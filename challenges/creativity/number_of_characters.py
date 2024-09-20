c1fc3708508a2838c5da7d28e09dcd7aa3506ffb
Kerriea-star <ojared511@gmail.com>
2024-09-20 13:38:24 +0300

    function to count the number of vowels in a given character string

--- challenges/creativity/number_of_characters.py
+++ challenges/creativity/number_of_characters.py
@@ -2,3 +2,12 @@
 Write a short Python function that counts the number of vowels in a given
 character string.
 """
+def count_vowels(character_string):
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    count = 0
+    for char in character_string.lower():
+        if char in vowels:
+            count += 1
+    return count
+
+print(count_vowels("Jared"))
