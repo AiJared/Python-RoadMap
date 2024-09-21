f6323f7e09ab42dcd0b5200002e5a22689a2214b
Kerriea-star <ojared511@gmail.com>
2024-09-21 08:51:41 +0300

    function to return a copy of the provided string without punctuations

--- challenges/creativity/string_copy.py
+++ challenges/creativity/string_copy.py
@@ -4,3 +4,13 @@ and returns a copy of the string with all punctuation removed.
 For example, if given the string "Let s try, Mike.", this function would return
 "Lets try Mike".
 """
+
+def string_copy(string):
+    punctuations = ["?", "!", ",", ".", "'"]
+    result = ""
+    for char in string:
+        if char not in punctuations:
+            result += char
+    return result
+
+print(string_copy("Let's try, Mike."))
