dd44f3285ea2853ceaa202cbd298c616a325062c
Kerriea-star <ojared511@gmail.com>
2024-09-21 08:40:56 +0300

    function to return a copy of the provided string without punctuations

---
+++ challenges/creativity/string_copy.py
@@ -0,0 +1,6 @@
+"""
+Write a short Python function that takes a string s, representing a sentence,
+and returns a copy of the string with all punctuation removed. 
+For example, if given the string "Let s try, Mike.", this function would return
+"Lets try Mike".
+"""
