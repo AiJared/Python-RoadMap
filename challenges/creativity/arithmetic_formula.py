10728a414e98a6000efe474a832b7a80c9d84749
Kerriea-star <ojared511@gmail.com>
2024-09-23 10:16:01 +0300

    function to determine if three integers can be used in a correct arithmetic formula

---
+++ challenges/creativity/arithmetic_formula.py
@@ -0,0 +1,5 @@
+"""
+Write a short program that takes as input three integers, a, b, and c, from
+the console and determines if they can be used in a correct arithmetic
+formula (in the given order), like “a+ b = c,” “a = b− c,” or “a∗b = c.”
+"""
