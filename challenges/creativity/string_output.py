393fbf0f304eeec98a8a1a21c2fd3d9193ca2077
Kerriea-star <ojared511@gmail.com>
2024-09-25 10:10:45 +0300

    function to output all possible strings formed by given characters

--- challenges/creativity/string_output.py
+++ challenges/creativity/string_output.py
@@ -2,3 +2,12 @@
 Write a Python program that outputs all possible strings formed by using
 the characters c , a , t , d , o , and g exactly once.
 """
+from itertools import permutations
+
+def string_output():
+    characters = ["c", "a", "t", "d", "o", "g"]
+
+    for perm in permutations(characters):
+        print(''.join(perm))
+
+string_output() 
