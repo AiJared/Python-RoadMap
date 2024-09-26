6b1bce0b055f3cbd3ef7e86072e129b71c78174f
Kerriea-star <ojared511@gmail.com>
2024-09-26 10:35:17 +0300

    function to take a positive integer greater than 2 as input then return the number of times it must be divided by 2 before getting a value less than 2

--- challenges/creativity/positive_integer.py
+++ challenges/creativity/positive_integer.py
@@ -3,3 +3,19 @@ Write a Python program that can take a positive integer greater than 2 as
 input and write out the number of times one must repeatedly divide this
 number by 2 before getting a value less than 2
 """
+
+def positive_integer():
+    value = int(input("Enter a positive integer greater than 2: "))
+    count = 0
+
+    if value > 2:
+        while value >= 2:
+            value = value / 2
+            count += 1
+    else:
+        print("Please provide a number greater than 2")
+
+    return count
+
+result = positive_integer()
+print(f"The number of divisions by 2 before getting a value less than 2 is: {result}")
