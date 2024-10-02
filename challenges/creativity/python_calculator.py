1e4adaf5fa2a167fba8014d55dda5f8e772cb257
Kerriea-star <ojared511@gmail.com>
2024-10-02 10:47:01 +0300

    python function that simulates a simple calculator

---
+++ challenges/creativity/python_calculator.py
@@ -0,0 +1,7 @@
+"""
+Write a Python program that can simulate a simple calculator, using the
+console as the exclusive input and output device. That is, each input to the
+calculator, be it a number, like 12.34 or 1034, or an operator, like + or =,
+can be done on a separate line. After each such input, you should output
+to the Python console what would be displayed on your calculator.
+"""
