1259971875862cbea20aca981b04392df1c2a96a
Kerriea-star <ojared511@gmail.com>
2024-10-03 10:42:39 +0300

    Python function that simulates a hand held calculator

---
+++ challenges/creativity/hand_held_calculator.py
@@ -0,0 +1,7 @@
+"""
+Write a Python program that simulates a handheld calculator. 
+Your program should process input from the Python console representing buttons
+that are “pushed,” and then output the contents of the screen after each operation is performed. 
+Minimally, your calculator should be able to process
+the basic arithmetic operations and a reset/clear operation.
+"""
