f8f201cc8dfbafd59d039c9a6e668502893624e9
Kerriea-star <ojared511@gmail.com>
2024-10-03 10:50:05 +0300

    Python function that simulates a hand held calculator

--- challenges/creativity/hand_held_calculator.py
+++ challenges/creativity/hand_held_calculator.py
@@ -5,3 +5,13 @@ that are “pushed,” and then output the contents of the screen after each ope
 Minimally, your calculator should be able to process
 the basic arithmetic operations and a reset/clear operation.
 """
+# Steps
+
+"""
+The simulated hand held calculator in the program below can handle basic arithmetic operations
+(addition, subtraction, multiplication, division). The program will take user inputs representing
+the buttons pushed (numbers and operators) and display results after each operation.
+1. Users enter button presses as strings(e.g. 5 + 3 * 2)
+2. The calculator evaluates the inputs after user presses the "Enter" key.
+3. The user can type "c" or "clear" to reset the calculator.
+"""
