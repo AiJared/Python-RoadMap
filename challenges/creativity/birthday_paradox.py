9ae66ff02695793e2ff98b86e4a187a928d09828
Kerriea-star <ojared511@gmail.com>
2024-10-05 10:16:33 +0300

    function to simulate birthday paradox

--- challenges/creativity/birthday_paradox.py
+++ challenges/creativity/birthday_paradox.py
@@ -15,3 +15,17 @@ def has_duplicates(birthdays):
 def generate_birthdays(n):
     # Generate n random birthdays, with each birthday being a number between 1 and 365
     return [random.randint(1, 365) for _ in range(n)]
+
+def birthday_paradox_simulation():
+    experiments = 1000 # Number of experiments to run for each n
+    for n in range(5, 101, 5):
+        duplicate_count = 0
+        for _ in range(experiments):
+            birthdays = generate_birthdays(n)
+            if has_duplicates(birthdays):
+                duplicate_count += 1
+    
+    probability = duplicate_count / experiments
+    print(f"For n = {n}, probability that two people have same birthday: {probability:.2f}")
+
+birthday_paradox_simulation()
