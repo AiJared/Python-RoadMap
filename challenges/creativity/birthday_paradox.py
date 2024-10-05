170c18e8a63f2a21cd0042549981b53a487ff3fa
Kerriea-star <ojared511@gmail.com>
2024-10-05 10:02:41 +0300

    birthday paradox

---
+++ challenges/creativity/birthday_paradox.py
@@ -0,0 +1,8 @@
+"""
+The birthday paradox says that the probability that two people in a room
+will have the same birthday is more than half, provided n, the number of
+people in the room, is more than 23. This property is not really a paradox,
+but many people find it surprising. Design a Python program that can test
+this paradox by a series of experiments on randomly generated birthdays,
+which test this paradox for n = 5,10,15,20,... ,100.
+"""
