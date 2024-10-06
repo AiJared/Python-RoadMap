37e97b5b13ecc774a44da919b8493abb4c5dd42e
Kerriea-star <ojared511@gmail.com>
2024-10-06 11:53:46 +0300

    function to return sum of elements of a list of integers

--- challenges/reinforcement/sum_of_lists.py
+++ challenges/reinforcement/sum_of_lists.py
@@ -4,10 +4,10 @@ Write a function that takes a list of integers then returns the sum of its eleme
 """
 
 def sum_of_list(lst):
-    total = 0
+    total = 0            # Initialize a variable to hold the sum
 
-    for num in lst:
-        total += num
-    return total
+    for num in lst:      # Loop through each element in the list
+        total += num     # Add each element to the total sum
+    return total         # Return the sum of all the elements
 
 print(sum_of_list([1,2,3,4]))
