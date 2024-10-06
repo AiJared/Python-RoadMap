193f758213b368894090cce7a839d8ea59e396e0
Kerriea-star <ojared511@gmail.com>
2024-10-06 12:11:52 +0300

    function to find the minimum value in a list

--- challenges/reinforcement/find_min.py
+++ challenges/reinforcement/find_min.py
@@ -3,12 +3,12 @@
 Write a python function that finds the minimum value in a list.
 """
 def find_min(lst):
-    min_value = lst[0]
+    min_value = lst[0]              # Assume the first element of the list is the smallest initially
 
-    for num in lst:
-        if num < min_value:
-            min_value = num
+    for num in lst:                 # Loop through the elements of the list
+        if num < min_value:         # If you find a value smaller than min value
+            min_value = num         # Update min value
     
-    return min_value
+    return min_value                # Return the minimum value       
 
 print(find_min([2,1,3,4,5]))
