5b481c6516cdecc8a8b65fede39678b2c0df9cc6
Kerriea-star <ojared511@gmail.com>
2024-10-06 12:00:08 +0300

    function to find the maximum value in a list

--- challenges/reinforcement/find_max.py
+++ challenges/reinforcement/find_max.py
@@ -3,3 +3,12 @@
 Write a function to find the maximum value in a list. However, do not use the built-in max() function
 apply the logic on your own.
 """
+def find_max(lst):
+    max_value = lst[0]
+
+    for num in lst:
+        if num > max_value:
+            max_value = num
+    return max_value
+
+print(find_max([1,2,3,4]))
