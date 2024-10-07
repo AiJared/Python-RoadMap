15345555235796e16be536e4f5f10b2bcd0194ad
Kerriea-star <ojared511@gmail.com>
2024-10-07 09:58:37 +0300

    function to merge to lists sorted in ascending order into a singel list sorted in ascending order

--- challenges/creativity/merge_lists.py
+++ challenges/creativity/merge_lists.py
@@ -2,4 +2,8 @@
 """
 Write a Python function that takes two sorted lists (in ascending order) and merges them into a
 single sorted list(also in ascending order)
+
+For example:
+input: [1,3,5] and [2,4,6]
+output: [1,2,3,4,5,6]
 """
