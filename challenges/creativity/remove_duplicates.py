64d160efebe01712096b7a6bbdc19538bd48b4a2
Kerriea-star <ojared511@gmail.com>
2024-10-08 09:26:21 +0300

    function that removes duplicates from a list

--- challenges/creativity/remove_duplicates.py
+++ challenges/creativity/remove_duplicates.py
@@ -3,3 +3,9 @@
 Write the Python function that takes a list as input then removes duplicates keeping the order
 of first occurrence of each element.
 """
+# Steps
+"""
+- You can use a loop to iterate through the list as you add elements into a new list if they aren't
+added already.
+- You a helper structure like set to track elements that have been added to the new list.
+"""
