eb05a8bd8f46177d4129ed8ead5d587524640ec6
Kerriea-star <ojared511@gmail.com>
2024-10-10 10:00:03 +0300

    function that reverses a list

--- challenges/reinforcement/reverse_list.py
+++ challenges/reinforcement/reverse_list.py
@@ -6,11 +6,11 @@ For example: Input [1,2,3,4,5], Output [5,4,3,2,1]
 # Using For Loop
 
 def reverse_list(n):
-    result_list = []
+    result_list = [] # Create an empty list to store elements of the reversed list
 
-    for i in range(len(n)-1,-1,-1):
-        result_list.append(n[i])
+    for i in range(len(n)-1,-1,-1): # Loop through the original list starting from the end
+        result_list.append(n[i]) # Add elements of the original list to the new list in reverse order
     
-    return result_list
+    return result_list # return the new reversed list
 
 print(reverse_list([1,2,3,4,5]))
