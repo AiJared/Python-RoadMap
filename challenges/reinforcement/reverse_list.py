e45a6993f19e49f497fff0b1e98dd3488048537b
Kerriea-star <ojared511@gmail.com>
2024-10-10 09:57:26 +0300

    function that reverses a list

--- challenges/reinforcement/reverse_list.py
+++ challenges/reinforcement/reverse_list.py
@@ -3,3 +3,14 @@
 Write a Python function that takes a list as inputs then returns its elements reversed as output:
 For example: Input [1,2,3,4,5], Output [5,4,3,2,1]
 """
+# Using For Loop
+
+def reverse_list(n):
+    result_list = []
+
+    for i in range(len(n)-1,-1,-1):
+        result_list.append(n[i])
+    
+    return result_list
+
+print(reverse_list([1,2,3,4,5]))
