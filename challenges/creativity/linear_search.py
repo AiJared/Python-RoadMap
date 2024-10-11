67a13c5cf2c36bb87627dcc83c07ad8c3af6d369
Kerriea-star <ojared511@gmail.com>
2024-10-11 10:44:46 +0300

    function that uses linear search to return index of a given target value

--- challenges/creativity/linear_search.py
+++ challenges/creativity/linear_search.py
@@ -1,6 +1,17 @@
-# task
+# Task
 """
 Write a Python function that takes a list and a target value as inputs then returns the index
 of the target value as output. If the target value does not exist, the function should just
 output -1
 """
+
+def linear_search(lst, target):
+    
+    for i in range(0, len(lst)):
+        if lst[i] == target:
+            return i
+    
+    return -1
+
+print(linear_search([10, 20, 30, 40, 50], 30)) 
+print(linear_search([10, 20, 30, 40, 50], 60)) 
