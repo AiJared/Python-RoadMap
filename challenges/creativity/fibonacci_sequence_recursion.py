e9e42ae88322f442925d8721c4986bdfed827c74
Kerriea-star <ojared511@gmail.com>
2024-10-13 10:09:01 +0300

    Fibonacci sequence using recursion

---
+++ challenges/creativity/fibonacci_sequence_recursion.py
@@ -0,0 +1,11 @@
+# Fibonacci Sequence Using Recursion
+"""
+A fibonacci sequence is a sequence of numbers where by each number (starting from the third) is
+the sum of previous two numbers.
+
+It goes like: 0,1,1,2,3,5,8,13,...
+
+Mathematically: F(n) = F(n - 1) + F(n - 2)
+
+Where: F(0) = 0 and F(1) = 1
+"""
