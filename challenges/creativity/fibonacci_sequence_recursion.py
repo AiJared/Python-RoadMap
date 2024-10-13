3c40d63e197bcb61d0cea7be49df21049a49ae94
Kerriea-star <ojared511@gmail.com>
2024-10-13 10:13:12 +0300

    Fibonacci sequence using recursion

--- challenges/creativity/fibonacci_sequence_recursion.py
+++ challenges/creativity/fibonacci_sequence_recursion.py
@@ -9,3 +9,16 @@ Mathematically: F(n) = F(n - 1) + F(n - 2)
 
 Where: F(0) = 0 and F(1) = 1
 """
+
+def fibonacci(n):
+    # Base case
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    
+    # Create recursive case
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+print(fibonacci(5))
