5f86225dd8a87489ee68100e913934c9ce60916d
Kerriea-star <ojared511@gmail.com>
2024-10-13 09:51:14 +0300

    Recursion

---
+++ challenges/creativity/recursion.py
@@ -0,0 +1,8 @@
+# What is Recursion?
+"""
+Recursion occurs when a function calls itself so that it can break problems into small
+manageable parts. There are two main components of recursion.
+1. Base case: A condition at which a recursion stops.
+2. Recursive case: The function calls itself with an argument that moves it towards the
+base case.
+"""
