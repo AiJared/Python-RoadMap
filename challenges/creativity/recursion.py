3ec4909a612cd0776067b6d6b9afd6cb8376bb37
Kerriea-star <ojared511@gmail.com>
2024-10-13 09:55:27 +0300

    Factorial as an example of implementation of recursion

--- challenges/creativity/recursion.py
+++ challenges/creativity/recursion.py
@@ -6,3 +6,15 @@ manageable parts. There are two main components of recursion.
 2. Recursive case: The function calls itself with an argument that moves it towards the
 base case.
 """
+
+# Factorial Example
+"""
+The factorial of a positive integer n is the product of all the positive integers that are
+less than or equal to n.
+
+n! = n * (n - 1) * (n - 2) * .... * 1
+
+That is:
+
+5! = 5 * 4 * 3 * 2 * 1 = 120
+"""
