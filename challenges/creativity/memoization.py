2ac65dde1af98f09dd1938fba6060ee8835fd1f4
Kerriea-star <ojared511@gmail.com>
2024-10-14 08:23:05 +0300

    Fibonacci sequence using memoization

--- challenges/creativity/memoization.py
+++ challenges/creativity/memoization.py
@@ -3,6 +3,10 @@
 Memoization is an optimization technique used to store the results of expensive function calls
 and then reuse them whenever the same inputs occur again. Instead of recalculating the Fibonacci
 of of a particular n multiple times, we store it once then reuse it.
+"""
 
-
+# Python Memoization using a Dictionary
+"""
+We can use a dictionary to store the results of a fibonacci calculation then check if the result
+of n is already computed before making a recursive call.
 """
