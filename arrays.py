eff02663374a017b3e9967d0edd9222dcf5ed281
Kerriea-star <ojared511@gmail.com>
2024-10-14 09:03:03 +0300

    Arrays, key features of arrays

---
+++ arrays.py
@@ -0,0 +1,11 @@
+# What is an Array
+"""
+An array is the simplest and most widely used data structure. It is a selection of components
+(values and variables) all of the same data type in a contingous memory location.
+
+Key Features:
+ - Fixed size:- The size of an array is specified at the time of its creation.
+ - Indexing:- Elements are accessed with an index number, with indexinf starting from 0
+ - Random access:- You can access elements at any position using their index numbers making
+ lookup fast, at O(1), that is, contant time.
+"""
