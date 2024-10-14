fd6ceedd9592225a301f84d87e6076555c83b058
Kerriea-star <ojared511@gmail.com>
2024-10-14 09:07:44 +0300

    operations of an array

--- arrays.py
+++ arrays.py
@@ -8,4 +8,11 @@ Key Features:
  - Indexing:- Elements are accessed with an index number, with indexinf starting from 0
  - Random access:- You can access elements at any position using their index numbers making
  lookup fast, at O(1), that is, contant time.
+
+Operations of an array
+ - Accessing: You can access elements by their index.
+ - Insertion: You can insert elements at the end or in the middle, but inserting in between
+ the array requires shifting elements, so it is less efficient.
+ - Deletion: Similar to insertion, deleting from beginning or in the middle requires
+ shifting elements.
 """
