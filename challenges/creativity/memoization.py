ead8505e561cd30e622a9399f11528c5abe17426
Kerriea-star <ojared511@gmail.com>
2024-10-14 08:39:13 +0300

    Fibonacci sequence using memoization

--- challenges/creativity/memoization.py
+++ challenges/creativity/memoization.py
@@ -10,3 +10,19 @@ of of a particular n multiple times, we store it once then reuse it.
 We can use a dictionary to store the results of a fibonacci calculation then check if the result
 of n is already computed before making a recursive call.
 """
+def fibonacci_memo(n, memo={}):
+    # Base cases
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    # check if n is in the memo dictionary
+    if n not in memo:
+        # calculate the fibonacci of n if it is not in memo
+        memo[n] = fibonacci_memo(n - 1, memo) + fibonacci_memo(n - 2, memo)
+    
+    return memo[n]
+
+print(fibonacci_memo(5))
+print(fibonacci_memo(10))
+print(fibonacci_memo(50))
