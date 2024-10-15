70b77f2b3cfe5261bd00f1a8c57fff48a4f67195
Kerriea-star <ojared511@gmail.com>
2024-10-15 10:37:23 +0300

    creating the attributes of the car class

--- OOP_Basics/class_objects.py
+++ OOP_Basics/class_objects.py
@@ -9,3 +9,6 @@ class Car:
     # method to describe the car
     def describe(self):
         return f"This car is {self.brand}, {self.model} manufactured in {self.year}"
+    
+# Creating the attributes of the class
+my_car = Car("Toyota", "Corolla", 2020)
