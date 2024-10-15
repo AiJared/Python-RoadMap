0332da037dec4eb86a1662efbd1afe230daa0dee
Kerriea-star <ojared511@gmail.com>
2024-10-15 10:34:36 +0300

    class Car

---
+++ OOP_Basics/class_objects.py
@@ -0,0 +1,11 @@
+# Define a class called car
+class Car:
+    # Create a constructor method to intialize the attributes (variables)
+    def __init__(self, brand, model, year):
+        self.brand = brand # attribute (variable) called brand
+        self.model = model # attribute called model
+        self.year = year # attribute called year
+
+    # method to describe the car
+    def describe(self):
+        return f"This car is {self.brand}, {self.model} manufactured in {self.year}"
