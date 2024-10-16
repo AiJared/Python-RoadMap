69efd4fe34a8ae8a3ddbe4603363d1603af34a7e
Kerriea-star <ojared511@gmail.com>
2024-10-16 10:33:32 +0300

    accessing private attributes of the class

--- OOP_Basics/encapsulation.py
+++ OOP_Basics/encapsulation.py
@@ -19,8 +19,8 @@ person1 = Person("Alice", 30)
 # Access attributes and methodes of the object person1
 print(person1.name)
 
-# trying to access the age of the object directly as shown below will raise an error
-# print(person1.age)
+# trying to access the age of the object directly as shown below will raise an Attribute error
+# print(person1.__age)
 
-# Instead access it via the get_age() method as shown below
+# Instead access it via the public get_age() method as shown below
 print(person1.get_age())
