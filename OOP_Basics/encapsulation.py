263787ef67f7a14c3e8316e05cd53fb26b61047a
Kerriea-star <ojared511@gmail.com>
2024-10-16 10:27:23 +0300

    class person demonstrating Encapsulation with public and private attributes

--- OOP_Basics/encapsulation.py
+++ OOP_Basics/encapsulation.py
@@ -1,13 +1,15 @@
 # Encapsulation with Public and Private Attributes
 class Person:
-
+    # Constructor initializes the attributes name and age
     def __init__(self, name, age):
         self.name = name # Public attribute
         self.__age = age # Private attribute
 
+    # method that describes the object "Person"
     def describe(self):
         return f"{self.name} is {self.__age} years old."
     
+    # Method to access the private attribute age
     def get_age(self):
         return self.__age
 
