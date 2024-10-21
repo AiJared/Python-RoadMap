965c59857a4e2d08a2fee5eef51c8b1f46e8de17
Kerriea-star <ojared511@gmail.com>
2024-10-21 09:05:46 +0300

    Concrete class Rectangle using the abstract methods from abstract base class Shape

--- OOP_Basics/abc.py
+++ OOP_Basics/abc.py
@@ -10,3 +10,15 @@ class Shape(ABC):
     @abstractmethod
     def perimeter(self):
         pass # Abstract method, no implementation
+
+# Concrete classes must implement abstract methods
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+    
+    def perimenter(self):
+        return 2 * (self.width + self.height)
