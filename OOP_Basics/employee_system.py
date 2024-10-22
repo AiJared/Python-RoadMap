5838e5b763cfe59773b3e8d5865ed0f65b2e93e1
Kerriea-star <ojared511@gmail.com>
2024-10-22 08:56:38 +0300

    FullTimeEmployee concrete class

--- OOP_Basics/employee_system.py
+++ OOP_Basics/employee_system.py
@@ -12,3 +12,16 @@ class Employee(ABC):
     def display_employee_details(self):
         pass
 
+# Concrete class FullTimeEmployee that inherits abstract class Employee
+class FullTimeEmployee(Employee):
+
+    def __init__(self, name, base_salary):
+        self.name = name
+        self.base_salary = base_salary
+    
+    # must implement abstract methods from abstract class Employee
+    def calculate_salary(self):
+        return self.base_salary
+    
+    def display_employee_details(self):
+        return f"{self.name}'s salary is {self.base_salary}"
