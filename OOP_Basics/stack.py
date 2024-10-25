85980974c76a2ff1148b69b21bda155aaef42ded
Kerriea-star <ojared511@gmail.com>
2024-10-25 09:03:36 +0300

    adding items on a stack

---
+++ OOP_Basics/stack.py
@@ -0,0 +1,9 @@
+# Stack Data Structure
+class Stack:
+    def __init__(self, item):
+        self.item = []
+    
+    # Push items on top of the stack
+    def add_item(self):
+        return self.item.append()
+    
