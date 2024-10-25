52b08c1db5f8a0be2c8dcac495ee13d1fc83d038
Kerriea-star <ojared511@gmail.com>
2024-10-25 09:06:06 +0300

    removing elements from the stack

--- OOP_Basics/stack.py
+++ OOP_Basics/stack.py
@@ -7,3 +7,10 @@ class Stack:
     def add_item(self):
         return self.item.append()
     
+    # Method to remove item ontop of the stack
+    def remove_item(self):
+        if not self.is_empty:
+            return self.item.pop()
+        else:
+            return f"Stack is empty!"
+    
